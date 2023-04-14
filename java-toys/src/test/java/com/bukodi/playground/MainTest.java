5b7c2578a007c24a4c118602df6b910b09b0fdd8
László Bukodi <bukodi@gmail.com>
2023-04-14 19:03:34 +0200

    Java PQC Kyber example added

--- java-toys/src/test/java/com/bukodi/playground/MainTest.java
+++
@@ -1,7 +0,0 @@
-package com.bukodi.playground;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class MainTest {
-
-}
---
+++ java-toys/src/test/java/com/bukodi/playground/pqckyber/TestPQCKyber.java
@@ -0,0 +1,11 @@
+package com.bukodi.playground.pqckyber;
+
+import org.junit.Test;
+
+public class TestPQCKyber {
+
+    @Test
+    public void testPQCKyber() throws Exception {
+
+    }
+}
