e78a570ee345254d54c0c686de1e75b9a1ee9474
Bukodi László <laszlo.bukodi@noreg.hu>
2023-05-08 17:36:16 +0200

    Spring boot with Webflux and R2DBC

---
+++ java-toys/src/main/java/com/bukodi/playground/spring/MyApplication.java
@@ -0,0 +1,14 @@
+package com.bukodi.playground.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Same as @SpringBootConfiguration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
