e7cccfcc89bdf6806e757b9bd48c88ee5bfa0b2f
László Bukodi <laszlo.bukodi@noreg.hu>
2025-11-07 15:29:48 +0100

    task test modified

--- go-toys/tctx/task_test.go
+++ go-toys/tctx/task_test.go
@@ -8,43 +8,82 @@ import (
 	"time"
 )
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"sync"
+	"testing"
+	"testing/synctest"
+	"time"
+)
+
 type singleThreadExecutor struct {
 	mu       sync.Mutex
 	ctx      context.Context
-	cancelFn context.CancelFunc
+	cancelFn context.CancelCauseFunc
 }
 
-var worker = singleThreadExecutor{}
-
-func (ste *singleThreadExecutor) Execute(parentCtx context.Context, f func(ctx context.Context), timeout time.Duration) {
-	ste.mu.Lock()
-	ste.ctx, ste.cancelFn = context.WithTimeoutCause(parentCtx, timeout, fmt.Errorf("timeout"))
-	context.AfterFunc(ste.ctx, func() {
-		fmt.Printf("after func")
-	})
-	ste.mu.Unlock()
-	f(ste.ctx)
-	ste.mu.Lock()
-	ste.cancelFn()
-	ste.mu.Unlock()
-}
-
-func (ste *singleThreadExecutor) Stop() {
-	ste.mu.Lock()
-	ste.cancelFn()
-	ste.mu.Unlock()
-}
-
-func TestExecuteTask(t *testing.T) {
-	worker.Execute(t.Context(), func(ctx context.Context) {
-		t.Logf("Task started")
+func (e *singleThreadExecutor) Execute(parentCtx context.Context, timeout time.Duration, task func(ctx context.Context) error) error {
+	e.mu.Lock()
+	if e.ctx != nil {
+		//
+		e.cancelFn(fmt.Errorf("task caceled"))
 		select {
-		case <-ctx.Done():
-			t.Logf("Task canceled")
-			return
-		case <-time.Tick(2 * time.Second):
-			t.Logf("Task finished")
+		case <-e.ctx.Done():
+			slog.DebugContext(e.ctx, "Task canceled after the user restarts")
+		case <-time.After(10 * time.Second):
+			slog.ErrorContext(e.ctx, "Waiting for task canceled timed out")
 		}
-	}, 1*time.Second)
-	worker.Stop()
+		e.ctx = nil
+		e.cancelFn = nil
+	}
+	ctx1, _ := context.WithTimeoutCause(parentCtx, timeout, errors.New("timeout"))
+	e.ctx, e.cancelFn = context.WithCancelCause(ctx1)
+	e.mu.Unlock()
+
+	ret := task(e.ctx)
+
+	e.mu.Lock()
+	e.cancelFn(nil)
+	e.ctx = nil
+	e.cancelFn = nil
+	e.mu.Unlock()
+	return ret
+}
+
+var testTask = func(ctx context.Context, dur time.Duration) error {
+	slog.Info("Task started")
+	select {
+	case <-ctx.Done():
+		slog.Info(fmt.Sprintf("Context canceled with: %+v", ctx.Err()))
+		return ctx.Err()
+	case <-time.After(dur):
+		slog.Info("Task finished")
+		return nil
+	}
+}
+
+func TestExecuteSimple(t *testing.T) {
+	timeUnit := time.Second
+	ste := &singleThreadExecutor{}
+
+	synctest.Test(t, func(t *testing.T) {
+		var wg sync.WaitGroup
+		wg.Go(func() {
+			err := ste.Execute(t.Context(), 20*timeUnit, func(ctx context.Context) error {
+				return testTask(ctx, 10*timeUnit)
+			})
+			t.Logf("err = %v", err)
+		})
+		wg.Go(func() {
+			time.Sleep(2 * timeUnit)
+			err := ste.Execute(t.Context(), 20*timeUnit, func(ctx context.Context) error {
+				return testTask(ctx, 5*timeUnit)
+			})
+			t.Logf("err = %v", err)
+		})
+		wg.Wait()
+	})
 }
